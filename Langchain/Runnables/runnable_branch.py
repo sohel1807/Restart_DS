927eeece2b1c4e8242a0e4b43f92ed815f22dfe9
sohel <sohelmujawar1807@gmail.com>
2026-07-03 22:21:13 +0530

    LangChain Runnables

---
+++ Langchain/Runnables/runnable_branch.py
@@ -0,0 +1,34 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableParallel,RunnablePassthrough,RunnableLambda,RunnableBranch
+
+load_dotenv()
+
+model = ChatGroq(model_name="llama-3.3-70b-versatile")
+
+prompt1 = PromptTemplate(
+    template='Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Summarize the following text in just 100 words: \n {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+report_gen_chain = prompt1 | model | parser
+
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split())>300, prompt2 | model | parser),
+    RunnablePassthrough()
+)
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain)
+
+print(final_chain.invoke({'topic':'Russia vs Ukraine'}))
+
+
---
+++ Langchain/Runnables/runnable_parallel.py
@@ -0,0 +1,31 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableParallel
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Generate a tweet about {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate a Linkedin post about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatGroq(model_name="llama-3.3-70b-versatile")
+
+parser = StrOutputParser()
+
+parallel_chain = RunnableParallel({
+    'tweet': RunnableSequence(prompt1, model, parser),
+    'linkedin': RunnableSequence(prompt2, model, parser)
+})
+
+result = parallel_chain.invoke({'topic':'AI'})
+
+print(result['tweet'])
+print(result['linkedin'])
