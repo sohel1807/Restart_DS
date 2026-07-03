c61d17611688a7505623844e7ee5e9d4603d0cce
sohel <sohelmujawar1807@gmail.com>
2026-07-03 11:59:42 +0530

    LangChain Parallel Chains

---
+++ Langchain/Chains/parallel_chains.py
@@ -0,0 +1,66 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain_core.runnables import RunnableParallel
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model=ChatGroq(model_name="llama-3.3-70b-versatile")
+
+
+
+prompt1 = PromptTemplate(
+    template='Generate short and simple notes from the following text \n {text}',
+    input_variables=['text']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate 5 short question along with MCQs 4 options of each not actual answers from the following text \n {text}',
+    input_variables=['text']
+)
+
+prompt3 = PromptTemplate(
+    template='Merge the provided notes and quiz into a single document \n notes -> {notes} and quiz -> {quiz}',
+    input_variables=['notes', 'quiz']
+)
+
+parser = StrOutputParser()
+
+parallel_chain = RunnableParallel({
+    'notes': prompt1 | model | parser,
+    'quiz': prompt2 | model | parser
+})
+
+merge_chain = prompt3 | model | parser
+
+chain = parallel_chain | merge_chain
+
+text = """
+Support vector machines (SVMs) are a set of supervised learning methods used for classification, regression and outliers detection.
+
+The advantages of support vector machines are:
+
+Effective in high dimensional spaces.
+
+Still effective in cases where number of dimensions is greater than the number of samples.
+
+Uses a subset of training points in the decision function (called support vectors), so it is also memory efficient.
+
+Versatile: different Kernel functions can be specified for the decision function. Common kernels are provided, but it is also possible to specify custom kernels.
+
+The disadvantages of support vector machines include:
+
+If the number of features is much greater than the number of samples, avoid over-fitting in choosing Kernel functions and regularization term is crucial.
+
+SVMs do not directly provide probability estimates, these are calculated using an expensive five-fold cross-validation (see Scores and probabilities, below).
+
+The support vector machines in scikit-learn support both dense (numpy.ndarray and convertible to that by numpy.asarray) and sparse (any scipy.sparse) sample vectors as input. However, to use an SVM to make predictions for sparse data, it must have been fit on such data. For optimal performance, use C-ordered numpy.ndarray (dense) or scipy.sparse.csr_matrix (sparse) with dtype=float64.
+"""
+
+result = chain.invoke({'text':text})
+
+print(result)
+
+
--- Langchain/PROMPTS/prompt_ui.py
+++ Langchain/PROMPTS/prompt_ui.py
@@ -8,21 +8,21 @@ load_dotenv()
 
 st.header("LangChain Chat with HuggingFace Models")
 
-# llm = ChatGroq(
-#     model_name="llama-3.3-70b-versatile",
-#     temperature=0.3,
-#     max_completion_tokens=100,
-# )
+llm = ChatGroq(
+    model_name="llama-3.3-70b-versatile",
+    temperature=0.3,
+    max_completion_tokens=100,
+)
 
-llm=HuggingFaceEndpoint(
-    repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
-    task="text-generation"
-)
-model = ChatHuggingFace(
-    llm=llm
-)
+# llm=HuggingFaceEndpoint(
+#     repo_id="meta-llama/Meta-Llama-3-8B-Instruct",
+#     task="text-generation"
+# )
+# model = ChatHuggingFace(
+#     llm=llm
+# )
 user_input = st.text_input("Enter your text here:")
 
 if st.button("Submit"):
-    result = model.invoke(user_input)
+    result = llm.invoke(user_input)
     st.write(result.content)
